46b954a02e59936df35edd658127bde97bf86cd9
Ohad Lan <ohadlannn@gmail.com>
2022-10-06 00:46:10 +0300

    create seconde selector and fill it with api data. making the second selector to change the bootstrap card with given info. problem - under location there array with residents, cant make the img from individual resident (/character) appear  in the card

--- script.js
+++ script.js
@@ -11,8 +11,7 @@ secondSearch.style.visibility = "hidden";
 
 searchInput.addEventListener("change", async () => {
   let result = await getListByInput();
-  const list2 = await b(result);
-  console.log(list2);
+  getAll(result);
 });
 
 async function getListByInput() {
@@ -21,41 +20,88 @@ async function getListByInput() {
   return result;
 }
 
-async function changeSearchTwo(list) {
+async function changeSearchTwo() {
   secondSearch.style.visibility = "visible";
 }
 
-// async function b(res) {
-//   list = [...list, ...res.results];
-//   console.log(list);
-//   if (res.info.next === null) {
-//     return [...list, ...res.results];
-//   } else if (res.info.next !== null) {
-//     fetch(`${res.info.next}`)
-//       .then(async (response) => {
-//         response = await response.json();
-//         list = b(response);
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//       });
-//   }
-// }
-
-async function b(res) {
-  list = [...list, ...res.results];
-  if (res.info.next === null) {
-    return list;
-  } else if (res.info.next !== null) {
-    fetch(`${res.info.next}`)
-      .then(async (result) => {
-        result = await result.json();
-
-        b(result);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+async function getAll(result) {
+  let a;
+  list = [];
+  for (let i = 1; i <= result.info.count; i++) {
+    a = await fetch(`${BASE_API}/${searchInput.value}/${i}`);
+    list.push(await a.json());
   }
-  return list;
+  changeSearchTwo();
+  addNamesToSearchBar();
+}
+
+function addNamesToSearchBar() {
+  secondSearch.innerHTML = "";
+  list.map((ele) => {
+    secondSearch.innerHTML += `<option value="${ele.id}">${ele.name}</option>`;
+  });
+}
+
+secondSearch.addEventListener("change", () => {
+  if (searchInput.value === "character") {
+    charDate();
+  } else if (searchInput.value === "location") {
+    locationDate();
+  } else if (searchInput.value === "episode") {
+    episodeDate();
+  }
+});
+function charDate() {
+  let index = secondSearch.value - 1;
+  let html = `<img src="${list[index].image}" class="card-img-top" alt="..." />
+  <div class="card-body">
+    <h5 class="card-title">name: ${list[index].name}</h5>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">status: ${list[index].status}</li>
+    <li class="list-group-item">species: ${list[index].species}</li>
+    <li class="list-group-item">gender: ${list[index].gender}</li>
+  </ul>
+  `;
+  searchResult.innerHTML = html;
+}
+async function locationDate() {
+  let index = secondSearch.value - 1;
+
+  let html = `<div id="search-result" class="card" style="width: 18rem">
+  <div class="card-body">
+    <h5 class="card-title">name: ${list[index].name}</h5>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">type: ${list[index].type}</li>
+    <li class="list-group-item">dimension: ${list[index].dimension}</li>
+  </ul>
+  <h6>residents: </h6>
+  <div>
+    ${await fillResidents(list[index].residents)}
+  </div>
+</div>`;
+  searchResult.innerHTML = html;
+}
+function episodeDate() {
+  console.log(secondSearch);
+}
+
+async function fillResidents(arr) {
+  let char;
+  let html = "";
+  if (arr === [] || arr === null) {
+    return "none";
+  } else {
+    await arr.map(async (ele) => {
+      char = await fetch(ele);
+      console.log(ele);
+      char = await char.json();
+      console.log(char);
+      html += `<img src="${char.image}" class="card-img-top" alt="..."/>`;
+      console.log(html); // good
+    });
+    console.log(html); // output => ""
+    return html;
+  }
 }
